b6a64e07e116a53ca55861f8306d5d37c32a323d
Lost Huskies <47117308+LostHuskies@users.noreply.github.com>
2026-07-09 04:14:13 -0400

    Update config_flow.py

--- custom_components/govee_thermometer/config_flow.py
+++ custom_components/govee_thermometer/config_flow.py
@@ -9,7 +9,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.config_entries import ConfigFlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import GoveeApi, GoveeAuthError, GoveeApiError
@@ -33,7 +33,7 @@ class GoveeThermometerConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    ) -> ConfigFlowResult:
         errors: dict[str, str] = {}
 
         if user_input is not None:
