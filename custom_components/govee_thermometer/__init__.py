08548f6415682f849aa2cebac215c3d9aa6afbde
Lost Huskies <47117308+LostHuskies@users.noreply.github.com>
2026-07-09 04:47:12 -0400

    Update __init__.py

--- custom_components/govee_thermometer/__init__.py
+++ custom_components/govee_thermometer/__init__.py
@@ -41,8 +41,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinators
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    
-    entry.async_on_unload(entry.add_to_updates_tracker(_async_options_updated))
 
     entry.async_on_unload(entry.add_update_listener(_async_options_updated))
     return True
